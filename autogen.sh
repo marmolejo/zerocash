3d427de2dcde917369f442bb0a5b009d2fc465ff
Zeus Gómez Marmolejo <zeus@aluzina.org>
2015-08-18 01:19:45 +0200

    Add debug mode

--- autogen.sh
+++ autogen.sh
@@ -6,17 +6,26 @@ autoreconf --install --force
 
 echo "Building dependencies..."
 
+if [ $# -ge 1 ]; then
+  if [ $1 = "debug" ]; then
+    echo "Setting debug flags"
+    DEBUG=debug
+    CONFDBG="--enable-debug=yes"
+  fi
+fi
+
 cd libzerocash/
-./get-libsnark
-make
+./get-libsnark $DEBUG
+make $DEBUG
 cd ..
 
 LD_LIBRARY_PATH=$srcdir/libzerocash/depinst/lib \
 INCLUDES="-I$srcdir/libzerocash \
   -I$srcdir/libzerocash/depinst/include \
   -I$srcdir/libzerocash/depinst/include/libsnark" \
-CXXFLAGS="-std=c++11 -DUSE_ASM -DMONTGOMERY_OUTPUT -DCURVE_BN128 -DBN_SUPPORT_SNARK" \
+CXX="g++ -std=c++11 -DUSE_ASM -DMONTGOMERY_OUTPUT -DCURVE_BN128 \
+  -DBN_SUPPORT_SNARK" \
 LIBS="-L$srcdir/libzerocash \
   -L$srcdir/libzerocash/depinst/lib \
   -lzerocash -lcryptopp -lgmp -lsnark" \
-./configure --with-incompatible-bdb --with-cli=no
+./configure --with-incompatible-bdb --with-cli=no $CONFDBG
