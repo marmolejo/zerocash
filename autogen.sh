a52ac492a648a94d3e213dc621ad319cdecfdcb8
Zeus Gómez Marmolejo <zeus@aluzina.org>
2015-08-17 17:23:17 +0200

    libzerocash dependency as git submodule, with compilation scripts

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 set -e
-srcdir="$(dirname $0)"
+srcdir="$(readlink -e $(dirname $0))"
 cd "$srcdir"
 autoreconf --install --force
+
+echo "Building dependencies..."
+
+cd libzerocash/
+./get-libsnark
+make
+cd ..
+
+LD_LIBRARY_PATH=$srcdir/libzerocash/depinst/lib \
+INCLUDES="-I$srcdir/libzerocash \
+  -I$srcdir/libzerocash/depinst/include \
+  -I$srcdir/libzerocash/depinst/include/libsnark" \
+CXXFLAGS="-std=c++11 -DUSE_ASM -DMONTGOMERY_OUTPUT -DCURVE_BN128 -DBN_SUPPORT_SNARK" \
+LIBS="-L$srcdir/libzerocash \
+  -L$srcdir/libzerocash/depinst/lib \
+  -lzerocash -lcryptopp -lgmp -lsnark" \
+./configure --with-incompatible-bdb --with-cli=no
