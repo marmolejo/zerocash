e02890d9f679cbe4136fc837a5bc4a7f1959573e
Zeus Gómez Marmolejo <zeus@aluzina.org>
2015-08-21 21:35:48 +0200

    Change CFLAGS to compile using C++14 standard

--- autogen.sh
+++ autogen.sh
@@ -23,7 +23,7 @@ LD_LIBRARY_PATH=$srcdir/libzerocash/depinst/lib \
 INCLUDES="-I$srcdir/libzerocash \
   -I$srcdir/libzerocash/depinst/include \
   -I$srcdir/libzerocash/depinst/include/libsnark" \
-CXX="g++ -std=c++11 -DUSE_ASM -DMONTGOMERY_OUTPUT -DCURVE_BN128 \
+CXX="g++ -std=c++14 -DUSE_ASM -DMONTGOMERY_OUTPUT -DCURVE_BN128 \
   -DBN_SUPPORT_SNARK" \
 LIBS="-L$srcdir/libzerocash \
   -L$srcdir/libzerocash/depinst/lib \
